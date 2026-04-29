f15addb272c1e5df99bfda7b37a88e324a4e2f53
Sanch0-0 <fff.alex.er.fff@gmail.com>
2026-04-29 15:44:11 +0600

    fix: CONTROLPLANE_IP changed to CONTROLPLANE_URL

--- agent/config.py
+++ agent/config.py
@@ -2,8 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    CONTROLPLANE_IP: str  # for firewall rule
-
+    CONTROLPLANE_URL: str  # for firewall rule
     HOST: str = "127.0.0.1"
     PORT: int = 9000
     WG_INTERFACE: str = "wg0"
@@ -11,6 +10,7 @@ class Settings(BaseSettings):
 
     class Config:
         env_file = ".env"
+        extra = "ignore"
 
 
 settings = Settings()
